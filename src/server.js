ad1d017d4042468d35e591350b4f145721c8f497
isaias <104476799+mrza1127@users.noreply.github.com>
2022-10-04 20:23:14 -0500

    hardcoded mongodb route

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3001
 
 const mongoose = require("mongoose")
 
+
 app.use(routes);
 
 app.use(express.urlencoded({ extended: true }));
@@ -15,9 +16,9 @@ app.use(express.static('dist'))
 main().catch(err => console.log(err))
 
 async function main () {
-    await mongoose.connect('mongodb://user:password@localhost:27017/testdb')
+    await mongoose.connect('mongodb://127.0.0.1:27017/testdb')
     app.listen(PORT, () => {
       console.log(`App running at http://localhost:${PORT}/`)
     })
     // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
-}
+  }
